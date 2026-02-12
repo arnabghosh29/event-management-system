f518c6b7eb8f4cea08e250e2fe8f66942354de24
Arnab Ghosh <138815328+arnabghosh29@users.noreply.github.com>
2026-02-12 21:57:51 +0530

    Configure server proxy for API requests
    Added server proxy configuration for API requests.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // সব /api/* request এখানে forward হবে
+      '/api': {
+        target: 'https://events.deepanwita.fun', // তোমার backend URL
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // /api remove করে backend এ যাবে
+      },
+    },
+  },
+});
